f3a3576704565af1b47e58b4fe586c5c1100f77f
ZaluzhnayaGalina <zaluzhnaya_galya@mail.ru>
2019-02-22 00:54:47 +0300

    Initial commit

---
+++ Asteroids/Asteroids/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Forms;
+
+namespace Asteroids
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var form = new Form();
+            form.Width = 800;
+            form.Height = 600;
+            form.Show();
+            Application.Run(form);
+        }
+    }
+}
