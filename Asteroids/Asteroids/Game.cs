975680a90a665b2b8165fdc50968b5a27db1b1d6
Zaluzhnaya Galina <zaluzhnaya_galya@mail.ru>
2019-02-22 10:42:15 +0300

    Timer and array of BaseObject in Game

--- Asteroids/Asteroids/Game.cs
+++ Asteroids/Asteroids/Game.cs
@@ -5,6 +5,7 @@ namespace Asteroids
 {
     public static class Game
     {
+        private static BaseObject[] _objects;
         private static BufferedGraphicsContext _context;
         public static BufferedGraphics Buffer;
         public static int Width { get; set; }
@@ -17,6 +18,24 @@ namespace Asteroids
             Width = form.ClientSize.Width;
             Height = form.ClientSize.Height;
             Buffer = _context.Allocate(g, new Rectangle(0, 0, Width, Height));
+            var timer = new Timer{Interval = 100};
+            timer.Start();
+            timer.Tick += Timer_Tick;
+        }
+
+        private static void Timer_Tick(object sender, System.EventArgs e)
+        {
+            Draw();
+            Update();
+        }
+
+        public static void Load()
+        {
+            _objects = new BaseObject[30];
+            for (int i = 0; i < _objects.Length; i++)
+            {
+                _objects[i] = new BaseObject(new Point(600, i*20),new Point(15-i,15-i), new Size(20,20)  );
+            }
         }
 
         public static void Draw()
@@ -25,6 +44,20 @@ namespace Asteroids
             Buffer.Graphics.DrawRectangle(Pens.White, new Rectangle(100,100,200,200));
             Buffer.Graphics.FillEllipse(Brushes.Wheat, new Rectangle(100,100,200,200));
             Buffer.Render();
+            Buffer.Graphics.Clear(Color.Black);
+            foreach (var baseObject in _objects)
+            {
+                baseObject.Draw();
+            }
+            Buffer.Render();
+        }
+
+        public static void Update()
+        {
+            foreach (var baseObject in _objects)
+            {
+                baseObject.Update();
+            }
         }
     }
 }
--- Asteroids/Asteroids/Program.cs
+++ Asteroids/Asteroids/Program.cs
@@ -10,6 +10,7 @@ namespace Asteroids
             form.Width = 800;
             form.Height = 600;
             Game.Init(form);
+            Game.Load();
             form.Show();
             Game.Draw();
             Application.Run(form);
