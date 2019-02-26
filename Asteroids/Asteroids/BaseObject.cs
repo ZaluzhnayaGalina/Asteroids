983dc02c0e37942fefcf74075b1248bc9b33a048
Zaluzhnaya Galina <zaluzhnaya_galya@mail.ru>
2019-02-26 14:12:15 +0300

    BaseObject - to abstract class

--- Asteroids/Asteroids/BaseObject.cs
+++ Asteroids/Asteroids/BaseObject.cs
@@ -2,26 +2,25 @@
 
 namespace Asteroids
 {
-    public class BaseObject
+    public abstract class BaseObject
     {
         protected Point Pos;
         protected Point Dir;
         protected Size Size;
 
-        public BaseObject(Point pos, Point dir, Size size)
+        protected BaseObject(Point pos, Point dir, Size size)
         {
             Pos = pos;
             Dir = dir;
             Size = size;
         }
 
-        public virtual void Draw()
-        {
-
-        }
+        public abstract void Draw();
 
         public virtual void Update()
         {
+            Pos.X = Pos.X + Dir.X;
+            if (Pos.X < 0) Pos.X = Game.Width + Size.Width;
 
         }
 
