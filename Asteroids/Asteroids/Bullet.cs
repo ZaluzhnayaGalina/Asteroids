bc256ad78f1e53f7edbb810148760bfe2534c0f5
Zaluzhnaya Galina <zaluzhnaya_galya@mail.ru>
2019-02-26 14:37:22 +0300

    Fix bullet update

--- Asteroids/Asteroids/Bullet.cs
+++ Asteroids/Asteroids/Bullet.cs
@@ -4,17 +4,22 @@ namespace Asteroids
 {
     public class Bullet: BaseObject
     {
+        private Point _startPos;
         public Bullet(Point pos, Point dir, Size size) : base(pos, dir, size)
         {
+            _startPos = new Point(pos.X, pos.Y);
         }
 
         public override void Draw()
         {
             Game.Buffer.Graphics.FillRectangle(Brushes.OrangeRed, Pos.X, Pos.Y, Size.Width, Size.Height);
         }
+
         public override void Update()
         {
-            Pos.X = Pos.X + 3;
+            base.Update();
+            if (Pos.X > Game.Width)
+                Pos.X = _startPos.X;
         }
     }
 }
